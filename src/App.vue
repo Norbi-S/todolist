ac828ca01c691adf5e9dab063625c9fad2430319
Norbi-S <nbitxd@gmail.com>
2020-06-13 00:00:50 +0200

    Item list setup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>app</div>
+  <v-app>
+    <v-app-bar app color="blue">
+      <v-toolbar-text>TodoList</v-toolbar-text>
+    </v-app-bar>
+    <v-main>
+      <TodoItemList :items="items" :doneItems="doneItems" />
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import TodoItemList from "./components/TodoItemList.vue";
+import ITodoItem from "./common/ITodoItem";
+
+type State = {
+  items: Array<ITodoItem>;
+};
 
 export default Vue.extend({
-  name: "App"
+  components: {
+    TodoItemList
+  },
+  data: function(): State {
+    return {
+      items: [
+        { id: 1, content: "First item", done: false },
+        { id: 2, content: "Second item", done: true }
+      ]
+    };
+  },
+  computed: {
+    doneItems: function(): Array<number> {
+      return this.items.filter(({ done }) => done).map(({ id }) => id);
+    }
+  }
 });
 </script>
---
+++ src/components/TodoItemList.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container fluid>
+    <p>items: {{ items }}</p>
+    <p>doneItems: {{ doneItems }}</p>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from "vue";
+import ITodoItem from "src/common/ITodoItem";
+
+export default Vue.extend({
+  props: {
+    items: {
+      type: Object as PropType<Array<ITodoItem>>
+    },
+    doneItems: {
+      type: Object as PropType<Array<number>>
+    }
+  }
+});
+</script>
