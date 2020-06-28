256bf1e2e2864c0cf4e5c1052129c9f69a0ebb65
Norbi-S <nbitxd@gmail.com>
2020-06-28 19:34:48 +0200

    Implement form validation

--- src/components/AddItemDialog.vue
+++ src/components/AddItemDialog.vue
@@ -1,5 +1,10 @@
 <template>
-  <v-dialog v-model="shown" fullscreen hide-overlay transition="dialog-bottom-transition">
+  <v-dialog
+    v-model="shown"
+    fullscreen
+    hide-overlay
+    transition="dialog-bottom-transition"
+  >
     <template v-slot:activator="{ on, attrs }">
       <v-btn fixed bottom right fab color="primary" v-bind="attrs" v-on="on">
         <v-icon>mdi-plus</v-icon>
@@ -13,15 +18,19 @@
         <v-toolbar-title class="white--text">Add Todo Item</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items>
-          <v-btn text @click="shown = false" color="white">Save</v-btn>
+          <v-btn text @click="onSave()" color="white">Save</v-btn>
         </v-toolbar-items>
       </v-toolbar>
       <v-card-text>
         <v-container fluid>
-          <v-form v-model="valid">
+          <v-form ref="form" v-model="valid" lazy-validation>
             <v-row>
               <v-container fluid>
-                <v-text-field label="Title" single-line :rules="titleRules"></v-text-field>
+                <v-text-field
+                  label="Title"
+                  single-line
+                  :rules="titleRules"
+                ></v-text-field>
               </v-container>
             </v-row>
           </v-form>
@@ -34,6 +43,14 @@
 <script lang="ts">
 import Vue from 'vue';
 
+interface FormRef {
+  validate: () => boolean;
+}
+
+function isFormRef(obj: unknown): obj is FormRef {
+  return typeof obj === 'object' && obj != null && 'validate' in obj;
+}
+
 export default Vue.extend({
   data: function () {
     return {
@@ -44,6 +61,18 @@ export default Vue.extend({
           (v != null && v.length > 0) || 'Title is required',
       ],
     };
+  },
+  methods: {
+    onSave() {
+      const formRef = this.$refs.form;
+      if (!isFormRef(formRef)) {
+        return;
+      }
+
+      if (formRef.validate()) {
+        this.shown = false;
+      }
+    },
   },
 });
 </script>
